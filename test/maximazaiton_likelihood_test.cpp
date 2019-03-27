dce4a1c3f48235fb1d6e72733626ab674357eb22
violetang <violetjiali@gmail.com>
2019-03-27 13:50:30 -0700

    bug fixed

--- test/maximazaiton_likelihood_test.cpp
+++ test/maximazaiton_likelihood_test.cpp
@@ -20,46 +20,61 @@ main(int argc, char** argv){
 
     int k = 10;     //mod->K
     float dest = 0;
-    int tag = 0;
+    int tag = 1;
     int source;     //Process sending the possibility 
-    MPI_Status status;;
+    MPI_Status status;
 
-    int max_k = 0;
+    int max_processor = 0;
     int max_logL = INT_MIN;
+    int amimax = 0; //int is 1 if processor is max, it's 0 otherwise
 
     //process do something and get a result for sending;
     srand(time(0)*my_rank);
-    float result = rand()/2;
+    float result = rand()/k;
    
     if(my_rank == 0){
-       // for(int countK = 0; countK < k; countK++ ){
-            
+       // for(int countK = 0; countK < k; countK++ ){           
             for(int proc = 1; proc < p; proc++){
                 cout<<"Process 0 is recieving "<<endl;
-                MPI_Recv(&result, 1, MPI_FLOAT, proc, tag, MPI_COMM_WORLD, &status);
+                MPI_Recv(&result, 1, MPI_FLOAT, proc, tag*proc, MPI_COMM_WORLD, &status);
                 if(max_logL < result){
                     max_logL = result;
-                    max_k = proc;
+                    max_processor = proc;
                 }
             }
             cout<<"the biggest one is"<<max_logL<<endl;
-            cout<<"the biggest p is"<<max_k<<endl;
-           // MPI_Send(&max_k,1,MPI_INT,max_k,tag,MPI_COMM_WORLD); //need change to send all
+            cout<<"the processor with biggest logL is"<<max_processor<<endl;
+            //Head node here knows which processor has the maxL
+            //So it has to send that info to every processor, so they can proceed
+            for (int proc = 1; proc < p; proc++){
+                if (proc == max_processor)
+                    amimax = 1;
+                MPI_Send(&amimax, 1, MPI_INT, proc, tag*proc*7, MPI_COMM_WORLD);
+            }
             MPI_Barrier(MPI_COMM_WORLD);  
        // }
     }
     else
     {
         cout<<"Process "<<my_rank<<" sending "<<endl;
-        MPI_Send(&result, 1, MPI_FLOAT, dest,tag, MPI_COMM_WORLD);
+        MPI_Send(&result, 1, MPI_FLOAT, dest, tag*my_rank, MPI_COMM_WORLD);
         cout<<"my result = " << result;
+
         MPI_Barrier(MPI_COMM_WORLD);
-        //MPI_Recv(&max_k,1,MPI_INT,dest,tag, MPI_COMM_WORLD);
-    
+        
+        MPI_Recv(&amimax, 1, MPI_INT, 0, tag*my_rank*7, MPI_COMM_WORLD, &status);
+        
+        if (amimax == 1)
+            cout<< "Hey I am processor "<<my_rank<<" and I have the max logl"<<endl;
+        else if (amimax == 0)
+            cout<< "I am processor "<<my_rank<<" and I don't have the max logl :("<<endl;
+        //MPI_Barrier(MPI_COMM_WORLD);
+        /*
         if(my_rank = max_k){
             //do printout job;
             cout << "!!! I'm the biggest" <<endl; 
         }
+        */
         
     }
 
